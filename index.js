96554fbc5cf02d428dc81dba25c1c49411a01a57
Vincent Tran <vincent.tran@qa.jbhunt.com>
2019-08-14 21:32:23 +0000

    Added a order function

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const Alpaca = require('@alpacahq/alpaca-trade-api');
 const dataKey = require('./data.json');
 
-const alpaca = new Alpaca(dataKey);
 
-alpaca.getAccount().then((account) => {
-    console.log('Current Account:', account)
-})
+class tradingBot{
+    constructor(){
+        this.alpaca = new Alpaca(dataKey);
+        this.alpaca.getAccount().then((account) => {
+            //console.log('Current Account:', account)
+        });
+    }
+    async submitOrder(quantity,stock,side) {
+        var prom = new Promise(async (resolve,reject) => {
+          if(quantity > 0){
+            await this.alpaca.createOrder({
+              symbol: stock,
+              qty: quantity,
+              side: side,
+              type: 'market',
+              time_in_force: 'day',
+            }).then(() => {
+              console.log("Market order of | " + quantity + " " + stock + " " + side + " | completed.");
+              resolve(true);
+            }).catch((err) => {
+                console.log(err);
+              console.log("Order of | " + quantity + " " + stock + " " + side + " | did not go through.");
+              resolve(false);
+            });
+          }
+          else {
+            console.log("Quantity is <=0, order of | " + quantity + " " + stock + " " + side + " | not sent.");
+            resolve(true);
+          }
+        });
+        return prom;
+    }
+}
+
+var bot = new tradingBot();
+bot.submitOrder("1","DAI","buy")
